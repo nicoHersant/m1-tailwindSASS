5e2824d88dcad9be8033394c7f865f7b2521c17c
nicoHersant <nicolas.hersant@gmail.com>
2026-01-30 13:25:01 +0100

    add grid layout

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+
